0dd4ffe6736c8c5f93a1481e44c3754a648bc277
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-11-21 18:53:02 -0600

    Update contracts code and scripts

--- near/5_contracts/assembly/index.ts
+++ near/5_contracts/assembly/index.ts
@@ -1,10 +1,10 @@
 import { context, storage, logging } from "near-sdk-as";
 
 export function getValue(): string | null {
-  return storage.getString("test");
+  return storage.getString("state");
 }
 
 export function setValue(value: string): void {
   logging.log("Setting value to " + value);
-  storage.setString("test", value);
+  storage.setString("state", value);
 }
